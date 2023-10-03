5020f3453b9d425b610f940276b915faab3959da
Matthias von Arx <matthias.von-arx@gmx.ch>
2023-10-03 22:06:17 +0200

    few fixes

--- UseNewModel.py
+++ UseNewModel.py
@@ -30,7 +30,7 @@ model = nn.Sequential(
     nn.Sigmoid()
 )
 
-model.load_state_dict(torch.load("NewModel"))
+model.load_state_dict(torch.load("model/NewModel"))
 
 X_test = TestSet[:,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22]]
 y_test = TestSet[:,16]
