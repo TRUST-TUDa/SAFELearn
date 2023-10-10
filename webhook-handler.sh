8b14c71f9870068a303e50a47065f67fe376ab4f
Matthias von Arx <matthias.von-arx@gmx.ch>
2023-10-10 12:46:57 +0200

    test

--- webhook-handler.sh
+++ webhook-handler.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Script started" >> /tmp/webhook.log
 # build the image
 docker build -t maettu102/safelearn:latest . --no-cache
 
 # push it
 docker push maettu102/safelearn:latest
+
