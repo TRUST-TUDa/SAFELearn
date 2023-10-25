38dd48f5582a4e0c6efe875d39957c5843b5a9de
Nicolas <balkenic@students.zhaw.ch>
2023-10-25 10:18:31 +0200

    cnn not working

---
+++ MNIST_CNN.py
@@ -0,0 +1,58 @@
+import numpy as np
+import keras 
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten
+from keras import backend as k
+
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+img_rows, img_cols=28, 28
+if k.image_data_format() == 'channels_first':
+   x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+   x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+   inpx = (1, img_rows, img_cols)
+ 
+else:
+   x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+   x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+   inpx = (img_rows, img_cols, 1)
+ 
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+
+
+
+inpx = Input(shape=inpx)
+layer1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(inpx)
+layer2 = Conv2D(64, (3, 3), activation='relu')(layer1)
+layer3 = MaxPooling2D(pool_size=(3, 3))(layer2)
+layer4 = Dropout(0.5)(layer3)
+layer5 = Flatten()(layer4)
+layer6 = Dense(250, activation='sigmoid')(layer5)
+layer7 = Dense(10, activation='softmax')(layer6)
+
+
+
+
+model = Model([inpx], layer7)
+model.compile(optimizer=keras.optimizers.Adadelta(),
+              loss=keras.losses.categorical_crossentropy,
+              metrics=['accuracy'])
+ 
+model.fit(x_train, y_train, epochs=12, batch_size=500)
+
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('loss=', score[0])
+print('acc=', score[1])
