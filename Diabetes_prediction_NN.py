ead605abfb4802b7e5952f0b7c91ea4fd743cb73
Matthias von Arx <matthias.von-arx@gmx.ch>
2023-10-29 18:15:09 +0100

    refactor diab

--- Diabetes_prediction_NN.py
+++ Diabetes_prediction_NN.py
@@ -2,9 +2,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data as loader
+import torch.utils.data as data
 import sys as sys
-import sklearn as skl
 import sklearn.metrics as sklm
 
 if(len(sys.argv)==1):
@@ -13,122 +12,117 @@ if(len(sys.argv)==1):
 
 version = sys.argv[1]
 
-def compute_recall(TP, FN):
-    return TP/ (TP + FN + 1e-8)
+# determine device (for gpu support)
+device = torch.device("cpu")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    torch.cuda.manual_seed(42)
+    torch.cuda.manual_seed_all(42)
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    torch.mps.manual_seed(42)
 
-def compute_precision(TP, FP):
-    return TP / (TP + FP + 1e-8)
-
-def compute_f1_score(precision, recall):
-    # Convert predictions to boolean values (0 or 1)
-    f1_score = 2 * (precision * recall) / (precision + recall + 1e-8)
-    return f1_score.item()
-
-
-# load the dataset, split into input (X) and output (y) variables
 fullset = np.loadtxt('data/Prepped_diabetes_data.data', delimiter=',')
-#TestSet = np.loadtxt('data/Prepped_diabetes_data.data', delimiter=',')
 
 generator1 = torch.Generator().manual_seed(42)
 generator2 = torch.Generator().manual_seed(42)
 torch.manual_seed(42)
 
+#data_loader = data.DataLoader(fullset, batch_size=100, shuffle=True)
 
 set_size = int(0.5 * len(fullset))
-SetGlobal, SetLocal = loader.random_split(fullset, [set_size, set_size], generator=generator1)
+SetGlobal, SetLocal = data.random_split(fullset, [set_size, set_size], generator=generator1)
 
 train_size = int(0.8 * len(SetGlobal))
 test_size = len(SetGlobal) - train_size
 
 if(version == "global"):
-    trainset, TestSet = loader.random_split(SetGlobal, [train_size, test_size], generator=generator1)
-    dataLoader = loader.DataLoader(SetGlobal)
+    trainset, TestSet = data.random_split(SetGlobal, [train_size, test_size], generator=generator1)
+    dataLoader = data.DataLoader(SetGlobal)
 if(version == "local"):
-    trainset, TestSet = loader.random_split(SetLocal, [train_size, test_size], generator=generator2)
-    dataLoader = loader.DataLoader(SetLocal)
+    trainset, TestSet = data.random_split(SetLocal, [train_size, test_size], generator=generator2)
+    dataLoader = data.DataLoader(SetLocal)
 
 
 
-X = trainset.dataset[:][:, :8]
-y = trainset.dataset[:][:,8]
+X_train = trainset.dataset[:][:, :8]
+y_train = trainset.dataset[:][:,8]
  
-X = torch.tensor(X, dtype=torch.float32)
-y = torch.tensor(y, dtype=torch.float32).reshape(-1, 1)
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
 
 X_test = TestSet.dataset[:][:, :8]
 y_test = TestSet.dataset[:][:,8]
 X_test = torch.tensor(X_test, dtype=torch.float32)
-y_test = torch.tensor(y_test, dtype=torch.int32).reshape(-1, 1)
- 
-# define the model
-model = nn.Sequential(
-    nn.Linear(8, 20),
-    nn.ReLU(),
-    nn.Linear(20, 10),
-    nn.ReLU(),
-    #nn.Linear(12, 7),
-    #nn.ReLU(),
-    nn.Linear(10, 4),
-    nn.ReLU(),
-    #nn.Softmax()
-    nn.Linear(4, 1),
-    nn.Sigmoid()
-)
+y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
+class DiabModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(8,20)
+        self.linear2 = nn.Linear(20,10)
+        self.linear3 = nn.Linear(10,4)
+        self.linear4 = nn.Linear(4,1)
+        self.act_fn = nn.ReLU()
+
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.act_fn(x)
+        x = self.linear2(x)
+        x = self.act_fn(x)
+        x = self.linear3(x)
+        x = self.act_fn(x)
+        x = self.linear4(x)
+        return x
+
+model = DiabModel()
 print(model)
 
 # train the model
-loss_fn   = nn.BCELoss()  # binary cross entropy    # np.MSELoss
-optimizer = optim.Adam(model.parameters(), lr=0.001) #try stuff
+model.train()
+loss_fn   = nn.BCEWithLogitsLoss()  # binary cross entropy with a sigmoid end layer (numerically more stable)  
+optimizer = optim.Adam(model.parameters(), lr=0.001) 
  
 n_epochs = 100
-batch_size = 100
+batch_size = 64
+
+model.to(device)
+
+def train_model(model,optimizer, X_train, y_train, loss_fn, n_epochs=100):
+    model.train()
  
-for epoch in range(n_epochs):
-    for i in range(0, len(X), batch_size): # make this random
-        Xbatch = X[i:i+batch_size]
+    for epoch in range(n_epochs):
+        for i in range(0, len(X_train), batch_size): # make this random
+            Xbatch = X_train[i:i+batch_size].to(device)
+            ybatch = y_train[i:i+batch_size].to(device)
+            y_pred = model(Xbatch)
+            loss = loss_fn(y_pred, ybatch)
+            optimizer.zero_grad() #Zero gradiants (necessary for backpropagation) - otherwise they are accumulated over time
+            loss.backward() # Backpropagation
+            optimizer.step() # Update parameters
+        print(f'Finished epoch {epoch}, latest loss {loss}')
+
+train_model(model,optimizer,X_train,y_train,loss_fn, n_epochs)
+
+def eval_model(model, X_test, y_test):
+    model.eval()
+
+    with torch.no_grad():
+        y_pred = model(X_test)
+        y_pred_binary = y_pred.round()
         
-        y_pred = model(Xbatch)
-        ybatch = y[i:i+batch_size]
-        loss = loss_fn(y_pred, ybatch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print(f'Finished epoch {epoch}, latest loss {loss}')
+        precision_sklm = sklm.precision_score(y_test, y_pred_binary)
+        recall_sklm = sklm.recall_score(y_test, y_pred_binary)
+        f1score_sklm = sklm.f1_score(y_test, y_pred_binary)
+        accuracy_sklm = sklm.accuracy_score(y_test, y_pred_binary)
+    
+        print("prec: ", precision_sklm)
+        print("recall: ", recall_sklm)
+        print("f1score: ", f1score_sklm)
+        print("accuracy ", accuracy_sklm)
 
-
-
-# compute accuracy (no_grad is optional)
-with torch.no_grad():
-    X_test
-    y_pred = model(X_test)
-    y_pred_binary = y_pred.round()
-    
-    #print(y_test)
-    #print(y_pred_binary)
-    accuracy = (y_pred_binary == y_test).float().mean()
-    TP = ((y_pred_binary == 1) & (y_test == 1)).float().sum()
-    FP = ((y_pred_binary == 1) & (y_test == 0)).float().sum()
-    FN = ((y_pred_binary == 0) & (y_test == 1)).float().sum()
-    
-    
-    
-    precision_sklm = sklm.precision_score(y_pred_binary, y_test)
-    recall_sklm = sklm.recall_score(y_pred_binary, y_test)
-    f1score_sklm = sklm.f1_score(y_pred_binary, y_test)
-    accuracy_sklm = sklm.accuracy_score(y_pred_binary, y_test)
-    
-    print("prec: ", precision_sklm)
-    print("recall: ", recall_sklm)
-    print("f1score: ", f1score_sklm)
-    print("accuracy ", accuracy_sklm)
-    
-    recall = compute_recall(TP, FN)
-    precision = compute_precision(TP,FP)
-    f1_score = compute_f1_score(precision, recall)
-    print(f"Accuracy mättu: {accuracy}")
-    print(f"F1 Score mättu: {f1_score}")
-    print(f"Recall mättu: {recall}")
-    print(f"Precision mättu: {precision}")
+eval_model(model,X_test, y_test)
 
 if(version =="global"):
     torch.save(model.state_dict(), "model/GlobalModel")
