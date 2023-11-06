919bc863c327a3bfbe37958b6bac05b3acfd80ad
Matthias von Arx <matthias.von-arx@gmx.ch>
2023-11-06 10:55:36 +0100

    general division

--- mpcaggregator/Aggregator.cpp
+++ mpcaggregator/Aggregator.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-ArithmeticShare perform_division(uint32_t bitlen, size_t number_of_elements, TypedArithmeticCircuit &ac,
+ArithmeticShare perform_division_average(uint32_t bitlen, size_t number_of_elements, TypedArithmeticCircuit &ac,
                                  TypedYaoCircuit &yc, size_t number_of_models, ArithmeticShare &summed_updates) {
     size_t boundary_for_negative_values = (1U << (bitlen - 1)) - 1;
     auto boundary_for_negative_values_a = ac->PutSIMDCONSGate(number_of_elements, boundary_for_negative_values, bitlen);
@@ -16,6 +16,16 @@ ArithmeticShare perform_division(uint32_t bitlen, size_t number_of_elements, Typ
     return ac->PutMUXGate(inverse_aggregated_update, aggregated_abs_update_a, is_negative);
 }
 
+ArithmeticShare perform_division(TypedArithmeticCircuit &ac,
+                                 TypedYaoCircuit &yc, const ArithmeticShare &dividend, const ArithmeticShare &divisor) {
+
+    YaoShare dividend_y = yc->PutA2YGate(dividend);
+    auto divisor_y = yc->PutA2YGate(divisor);
+    auto quotient_y = yc->divide(dividend_y, divisor_y);
+    return ac->PutY2AGate(quotient_y);
+}
+
+
 OUTPUT_NUMBER_TYPE *aggregate_models(MPCParty &party, uint32_t bitlen, size_t number_of_elements,
                                      vector<ArithmeticShare> *updates, TypedArithmeticCircuit &ac, TypedYaoCircuit &yc,
                                      const ArithmeticShare &global_model, const ArithmeticShare * q_vals) {
@@ -26,7 +36,7 @@ OUTPUT_NUMBER_TYPE *aggregate_models(MPCParty &party, uint32_t bitlen, size_t nu
 
     for (const auto &update : *updates)
     {
-        ArithmeticShare curr = ac->PutMULGate(update, q_vals[i++]);
+        ArithmeticShare curr = perform_division(ac, yc, update, q_vals[i++]);
         if (is_first_update) {
             summed_updates = curr;
             is_first_update = false;
@@ -34,7 +44,7 @@ OUTPUT_NUMBER_TYPE *aggregate_models(MPCParty &party, uint32_t bitlen, size_t nu
             summed_updates = ac->PutADDGate(summed_updates, curr);
         }
     }
-    ArithmeticShare aggregated_update = perform_division(bitlen, number_of_elements, ac, yc, updates->size(),
+    ArithmeticShare aggregated_update = perform_division_average(bitlen, number_of_elements, ac, yc, updates->size(),
                                                          summed_updates);
     ArithmeticShare aggregated_model = ac->PutADDGate(aggregated_update, global_model);
     UntypedSharedOutputShare output_share = ac->PutSharedOUTGate(aggregated_model);
