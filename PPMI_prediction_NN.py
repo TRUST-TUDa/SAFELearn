dab2ecaabfc246b5acb2a4885465175b1bea912c
Nicolas <balkenic@students.zhaw.ch>
2023-11-09 16:29:26 +0100

    PPMI model but broken

--- PPMI_prediction_NN.py
+++ PPMI_prediction_NN.py
@@ -25,13 +25,14 @@ generator = torch.Generator().manual_seed(42)
 torch.manual_seed(42)
 
 set_size = len(fullset)
-splits = []
+clients = []
+number_of_clients =1
 
 # Split the data into 100 non-overlapping parts
-split_size = len(fullset) // 100
-for i in range(100):
+split_size = len(fullset) // number_of_clients
+for i in range(number_of_clients):
     split = fullset[i * split_size: (i + 1) * split_size]
-    splits.append(split)
+    clients.append(split)
 
 n_epochs = 100
 batch_size = 64
@@ -44,7 +45,7 @@ class DiabModel(nn.Module):
         self.linear3 = nn.Linear(10,4)
         self.linear4 = nn.Linear(4,3)
         self.act_fn = nn.ReLU()
-        self.sigm = nn.Softmax()
+        self.sigm = nn.Sigmoid()
 
 
     def forward(self, x):
@@ -55,7 +56,7 @@ class DiabModel(nn.Module):
         x = self.linear3(x)
         x = self.act_fn(x)
         x = self.linear4(x)
-        x = self.sigm(x)
+        #x = self.sigm(x)
         return x
 
 
@@ -66,29 +67,29 @@ def eval_model(model, X_test, y_test):
     with torch.no_grad():
         y_pred = model(X_test)
         y_pred_integer = y_pred.round().cpu().numpy()
-        precision_sklm = sklm.precision_score(y_test, y_pred_integer, average='samples')
-        precision_sklm_inv = sklm.precision_score(y_pred_integer, y_test, average='samples')
-        recall_sklm = sklm.recall_score(y_test, y_pred_integer, average='samples')
-        recall_sklm_inv = sklm.recall_score(y_pred_integer,y_test, average='samples')
-        f1score_sklm = sklm.f1_score(y_test, y_pred_integer, average='samples')
-        f1score_sklm = sklm.f1_score(y_pred_integer,y_test, average='samples')
-        accuracy_sklm = sklm.accuracy_score(y_test, y_pred_integer, average='samples')
-
-        print("precision: ", precision_sklm)
-        print("precision inv:", precision_sklm_inv)
-        print("recall: ", recall_sklm)
-        print("recall: inv", recall_sklm_inv)
-        print("f1-score: ", f1score_sklm)
-        print("accuracy: ", accuracy_sklm)
+        #precision_sklm = sklm.precision_score(y_test, y_pred_integer, average ='samples', labels= ['1, 2, 4'])
+        #precision_sklm_inv = sklm.precision_score(y_pred_integer, y_test, average='samples')
+        #recall_sklm = sklm.recall_score(y_test, y_pred_integer, average='samples')
+        #recall_sklm_inv = sklm.recall_score(y_pred_integer,y_test, average='samples')
+        #f1score_sklm = sklm.f1_score(y_test, y_pred_integer, average='samples')
+        #f1score_sklm = sklm.f1_score(y_pred_integer,y_test, average='samples')
+        #accuracy_sklm = sklm.accuracy_score(y_test, y_pred_integer, average='samples')
+#
+        #print("precision: ", precision_sklm)
+        #print("precision inv:", precision_sklm_inv)
+        #print("recall: ", recall_sklm)
+        #print("recall: inv", recall_sklm_inv)
+        #print("f1-score: ", f1score_sklm)
+        #print("accuracy: ", accuracy_sklm)
 
 # if the global model does not yet exist create a new fully untrained one
-if not os.path.exists("model/PPMImodels/GlobalModel"):
-    model = DiabModel()
-    torch.save(model.state_dict(), f"model/PPMImodels/GlobalModel") 
+#if not os.path.exists("model/PPMImodels/GlobalModel"):
+#    model = DiabModel()
+#    torch.save(model.state_dict(), f"model/PPMImodels/GlobalModel") 
 
 
 if (len(sys.argv) == 1):
-    for split_index, split_data in enumerate(splits):
+    for split_index, split_data in enumerate(clients):
         train_size = int(0.8 * len(split_data))
         test_size = len(split_data) - train_size
         train_data, test_data = data.random_split(split_data, [train_size, test_size], generator=generator)
@@ -103,7 +104,7 @@ if (len(sys.argv) == 1):
 
         #model.load_state_dict(torch.load("model/PPMImodels/GlobalModel"))
         
-        loss_fn = nn.BCELoss()
+        loss_fn = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=0.001)
         def train_model(model, optimizer, X_train, y_train, loss_fn, n_epochs=100):
             model.train()
@@ -113,8 +114,7 @@ if (len(sys.argv) == 1):
                     Xbatch = X_train[i:i+batch_size].to(device)
                     ybatch = y_train[i:i+batch_size].to(device)
                     y_pred = model(Xbatch)
-
-                    loss = loss_fn(y_pred, ybatch)
+                    loss = loss_fn(y_pred, torch.reshape(ybatch, (-1,)).to(torch.int64))
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
