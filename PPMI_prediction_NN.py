fe4e5573c84b0d4522571d016870006d7230b778
balkenic <balkenic@students.zhaw.ch>
2023-11-09 10:13:25 +0100

    cleaned data further, made model for PPMI, model not working yet

---
+++ PPMI_prediction_NN.py
@@ -0,0 +1,137 @@
+import numpy as np
+import torch
+import pandas as pd
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import sys
+import sklearn.metrics as sklm
+import os
+
+torch.manual_seed(42)
+
+# Determine device (for GPU support)
+device = torch.device("cpu")
+#if torch.cuda.is_available():
+#    device = torch.device("cuda")
+#    torch.cuda.manual_seed(42)
+#    torch.cuda.manual_seed_all(42)
+
+print("Device:", device)
+fullset = pd.read_csv('data/PPMI/PPMI_cleaned.csv')
+fullset = torch.Tensor(fullset.to_numpy())
+
+generator = torch.Generator().manual_seed(42)
+torch.manual_seed(42)
+
+set_size = len(fullset)
+splits = []
+
+# Split the data into 100 non-overlapping parts
+split_size = len(fullset) // 100
+for i in range(100):
+    split = fullset[i * split_size: (i + 1) * split_size]
+    splits.append(split)
+
+n_epochs = 100
+batch_size = 64
+
+class DiabModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(12,20)
+        self.linear2 = nn.Linear(20,10)
+        self.linear3 = nn.Linear(10,4)
+        self.linear4 = nn.Linear(4,3)
+        self.act_fn = nn.ReLU()
+        self.sigm = nn.Softmax()
+
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.act_fn(x)
+        x = self.linear2(x)
+        x = self.act_fn(x)
+        x = self.linear3(x)
+        x = self.act_fn(x)
+        x = self.linear4(x)
+        x = self.sigm(x)
+        return x
+
+
+
+def eval_model(model, X_test, y_test):
+    model.eval()
+
+    with torch.no_grad():
+        y_pred = model(X_test)
+        y_pred_integer = y_pred.round().cpu().numpy()
+        precision_sklm = sklm.precision_score(y_test, y_pred_integer, average='samples')
+        precision_sklm_inv = sklm.precision_score(y_pred_integer, y_test, average='samples')
+        recall_sklm = sklm.recall_score(y_test, y_pred_integer, average='samples')
+        recall_sklm_inv = sklm.recall_score(y_pred_integer,y_test, average='samples')
+        f1score_sklm = sklm.f1_score(y_test, y_pred_integer, average='samples')
+        f1score_sklm = sklm.f1_score(y_pred_integer,y_test, average='samples')
+        accuracy_sklm = sklm.accuracy_score(y_test, y_pred_integer, average='samples')
+
+        print("precision: ", precision_sklm)
+        print("precision inv:", precision_sklm_inv)
+        print("recall: ", recall_sklm)
+        print("recall: inv", recall_sklm_inv)
+        print("f1-score: ", f1score_sklm)
+        print("accuracy: ", accuracy_sklm)
+
+# if the global model does not yet exist create a new fully untrained one
+if not os.path.exists("model/PPMImodels/GlobalModel"):
+    model = DiabModel()
+    torch.save(model.state_dict(), f"model/PPMImodels/GlobalModel") 
+
+
+if (len(sys.argv) == 1):
+    for split_index, split_data in enumerate(splits):
+        train_size = int(0.8 * len(split_data))
+        test_size = len(split_data) - train_size
+        train_data, test_data = data.random_split(split_data, [train_size, test_size], generator=generator)
+        
+        X_train = torch.tensor(train_data.dataset[:, 2:], dtype=torch.float32)
+        y_train = torch.tensor(train_data.dataset[:, 1], dtype=torch.float32).reshape(-1, 1)
+        X_test = torch.tensor(test_data.dataset[:, 2:], dtype=torch.float32)
+        y_test = torch.tensor(test_data.dataset[:, 1], dtype=torch.float32).reshape(-1, 1)
+        
+        model = DiabModel()
+        # if there exists a global model from earlier learnings import it
+
+        #model.load_state_dict(torch.load("model/PPMImodels/GlobalModel"))
+        
+        loss_fn = nn.BCELoss()
+        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        def train_model(model, optimizer, X_train, y_train, loss_fn, n_epochs=100):
+            model.train()
+            model.to(device)
+            for epoch in range(n_epochs):
+                for i in range(0, len(X_train), batch_size):
+                    Xbatch = X_train[i:i+batch_size].to(device)
+                    ybatch = y_train[i:i+batch_size].to(device)
+                    y_pred = model(Xbatch)
+
+                    loss = loss_fn(y_pred, ybatch)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                print(f'Split {split_index}, Epoch {epoch}, latest loss {loss}')
+            torch.save(model.state_dict(), f"model/PPMImodels/Model_{split_index}")
+        train_model(model, optimizer, X_train, y_train, loss_fn, n_epochs)
+        eval_model(model, X_test, y_test)
+# if a cli argument is given only evaluate
+else: 
+    if os.path.exists("model/PPMImodels/GlobalModel"):
+        model = DiabModel()
+        model.load_state_dict(torch.load("model/PPMImodels/GlobalModel"))
+        X_test_all = torch.tensor(fullset[:, 2:], dtype=torch.float32)
+        y_test_all = torch.tensor(fullset[:, 1], dtype=torch.float32).reshape(-1, 1)
+        eval_model(model, X_test_all, y_test_all)
+    else: 
+        print("no global model found")
+        quit()
+        
+        
--- data/PPMI/clean_curated.py
+++ data/PPMI/clean_curated.py
@@ -1,12 +1,45 @@
 import pandas as pd
 import numpy as np
+pd.options.mode.chained_assignment = None  # default='warn'
+
+
 df = pd.read_csv("data/PPMI/PPMI_Curated_Data_Cut_Public_20230612_rev.csv")
 
-columns_to_keep = ['PATNO', 'COHORT','age_at_visit', 'SEX', 'race', 'BMI', 'moca', 'fampd', 'bjlot' ]
-df[columns_to_keep] = df[columns_to_keep].replace('.', np.nan)
+columns_to_keep = ['PATNO', 'COHORT','age_at_visit', 'SEX', 'race', 'BMI', 'moca', 'fampd', 'bjlot', 'stai', 'scopa', 'CSFSAA', 'nfl_serum', 'asyn']
+#df[columns_to_keep] = df[columns_to_keep].replace('.', np.nan)
+
+
+average = []
+std = []
+
+for column in columns_to_keep:
+    curr_sum = 0
+    numbers_for_std = []
+    for i in range(len(df[column])):
+        if(df[column][i] == '.'):
+            continue
+        curr_sum += float(df[column][i])
+        numbers_for_std.append(float(df[column][i]))
+        
+    average.append(curr_sum / len(numbers_for_std))
+    #print(max(numbers_for_std))
+    std.append(np.std(numbers_for_std, ddof=1))
+    
+for column in columns_to_keep:
+    col_index = columns_to_keep.index(column)
+    for i in range(len(df[column])):
+        if(df[column][i] == '.'):
+            df[column][i] = round((average[col_index] + (np.random.uniform(-1, 1) * std[col_index])), 4)
+
+
 df = df.groupby('PATNO').first().reset_index()
 df = df[columns_to_keep]
+df.drop(columns=df.columns[0], axis=1, inplace=True)
 
-df = df.dropna(subset=columns_to_keep)
+#print(average[13])
+#print(std[13])
+
+
+#df = df.dropna(subset=columns_to_keep)
 print(df.head)
-df.to_csv("data/PPMI/cleaned.csv")
+df.to_csv("data/PPMI/PPMI_cleaned.csv")
